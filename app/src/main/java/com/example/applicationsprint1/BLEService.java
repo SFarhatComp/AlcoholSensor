66fb0ba6e8294c81734094e3c406592653511e4b
mailh <stefan.nechita@nitrado.net>
2022-11-03 02:33:29 -0400

    Sprint 1 final

---
+++ app/src/main/java/com/example/applicationsprint1/BLEService.java
@@ -0,0 +1,87 @@
+package com.example.applicationsprint1;
+
+import android.Manifest;
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothProfile;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+
+public class BLEService extends Service {
+    private Binder binder = new LocalBinder();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    class LocalBinder extends Binder {
+        public BLEService getService() {
+            return BLEService.this;
+        }
+    }
+
+    public static final String TAG = "BluetoothLeService";
+    private BluetoothGatt bluetoothGatt;
+    private BluetoothAdapter bluetoothAdapter;
+
+
+    public boolean initialize() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Log.e(TAG, "Unable to obtain a BluetoothAdapter.");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean connect(final String address) {
+        if (bluetoothAdapter == null || address == null) {
+            Log.w(TAG, "BluetoothAdapter not initialized or unspecified address.");
+            return false;
+        }
+
+        try {
+            final BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return false;
+            }
+            bluetoothGatt = device.connectGatt(this, false, bluetoothGattCallback);
+            return true;
+        } catch (IllegalArgumentException exception) {
+            Log.w(TAG, "Device not found with provided address.");
+            return false;
+        }
+        // connect to the GATT server on the device
+    }
+
+    private final BluetoothGattCallback bluetoothGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                // successfully connected to the GATT Server
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                // disconnected from the GATT Server
+            }
+        }
+    };
+
+
+}
--- app/src/main/java/com/example/applicationsprint1/MainActivity.java
+++ app/src/main/java/com/example/applicationsprint1/MainActivity.java
@@ -1,19 +1,43 @@
 package com.example.applicationsprint1;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.applicationsprint1.BLEService.LocalBinder;
 import com.example.applicationsprint1.database.AppDatabase;
 import com.example.applicationsprint1.database.entities.profile;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,64 +48,89 @@ public class MainActivity extends AppCompatActivity {
     protected ProfileRecyclerViewAdapter customRecyclerViewAdapter;
     protected AppDatabase db;
     protected List<profile> ListOfProfiles;
+    Context context;
+    public String deviceAddress;
 
+    private BLEService bluetoothService;
 
+  private ServiceConnection serviceConnection = new ServiceConnection() {
+    @Override
+      public void onServiceConnected(ComponentName name, IBinder service) {
+           bluetoothService = ((LocalBinder) service).getService();
+          if (bluetoothService != null) {
+              if (!bluetoothService.initialize()) {
+               Log.e(TAG, "Unable to initialize Bluetooth");
+                   finish();
+               }
+               bluetoothService.connect(deviceAddress);
+           }
+       }
+
+       @Override
+       public void onServiceDisconnected(ComponentName name) {
+           bluetoothService = null;
+       }
+  };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = getApplicationContext();
+        deviceAddress="50:65:83:90:05:55";
+        Intent gattServiceIntent = new Intent(this, BLEService.class);
+        bindService(gattServiceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+
         setContentView(R.layout.activity_main);
         SetUp(); // This function call for the init of the variables
-//
-        //This for loop is only to test valid input in the database profile
+
+       // This for loop is only to test valid input in the database profile
 //        for (int i=0;i<5;i++){
 //
-//            db.profileDao().insertProfile(new profile(0,"Sami","Farhat","male",185,120,23));
+  //          db.profileDao().insertProfile(new profile(0,"Sami","Farhat","male",185,120,23));
 //
 //
-//        }
+  //    }
 
-        ListOfProfiles=db.profileDao().getAll();
+        ListOfProfiles = db.profileDao().getAll();
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        customRecyclerViewAdapter= new ProfileRecyclerViewAdapter(ListOfProfiles);
+        customRecyclerViewAdapter = new ProfileRecyclerViewAdapter(ListOfProfiles);
         ListViewOfProfile.setLayoutManager(linearLayoutManager);
         ListViewOfProfile.setAdapter(customRecyclerViewAdapter);
-
     }
 
 
 
-
-void SetUp(){
+    void SetUp() {
         // set the variables equal to the correct items in the layout ;
-        DisplayButton_= findViewById(R.id.DisplayButton);
-        FragmentButton_=findViewById(R.id.FragmentButton);
-        ListViewOfProfile=findViewById(R.id.ListViewer);
-        InformationDisplays=findViewById(R.id.InformationDisplayer);
+        DisplayButton_ = findViewById(R.id.DisplayButton);
+        FragmentButton_ = findViewById(R.id.FragmentButton);
+        ListViewOfProfile = findViewById(R.id.ListViewer);
+        InformationDisplays = findViewById(R.id.TextView);
         db = AppDatabase.CreateDatabase(getApplicationContext());
-};
+    }
 
-void OnClicks(){
+    ;
 
-    FragmentButton_.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
+    void OnClicks() {
 
-
-            // This button should call a fragment that would allow a user to create a "profile:
-        }
-    });
+        FragmentButton_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
 
-    DisplayButton_.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
+                // This button should call a fragment that would allow a user to create a "profile:
+            }
+        });
 
-            // The display button will allow the user to change between two different views, either the display with last name, or the display with priorities in case of emergency
-        }
-    });
 
+        DisplayButton_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // The display button will allow the user to change between two different views, either the display with last name, or the display with priorities in case of emergency
+            }
+        });
 
 
 
